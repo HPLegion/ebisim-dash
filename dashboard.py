9f2bdbd7e689bb7b29c812d31d0a0db66c565d69
HPLegion <32222819+HPLegion@users.noreply.github.com>
2019-06-13 17:08:09 +0200

    Introduce clipping for distribution time control, clean up layout, add cross section plots, add proton numbers to elements, clean upp callbacks

--- dashboard.py
+++ dashboard.py
@@ -6,19 +6,34 @@ import math
 from functools import lru_cache
 
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 
 import numpy as np
 import ebisim
 
+#### Helper functions ####
+cached_get_element = lru_cache(maxsize=128)(ebisim.get_element)
 cached_basic_sim = lru_cache(maxsize=128)(ebisim.basic_simulation)
+lru_cache(maxsize=128)
+def cached_eixs(z, n=500):
+    return ebisim.eixs_energyscan(cached_get_element(z), n=n)
+lru_cache(maxsize=128)
+def cached_rrxs(z, n=500):
+    return ebisim.rrxs_energyscan(cached_get_element(z), n=n)
+lru_cache(maxsize=128)
+def cached_drxs(z, fwhm, n=500):
+    return ebisim.drxs_energyscan(cached_get_element(z), fwhm, n=n)
 
+def figure_to_data(figure):
+    return [(np.array(line["x"]), np.array(line["y"])) for line in figure["data"]]
+
+#### Page Content Preparation ####
 _BOOTSTRAP_CDN = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 _DISCLAIMER = 'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.'
 
-_ELEMENTS = [{"label":ebisim.elements.element_name(z), "value":z} for z in range(2, 106)]
+_ELEMENTS = [{"label":f"{z} - {ebisim.elements.element_name(z)}", "value":z} for z in range(2, 106)]
 
 _HEADER = html.Section(className="text-center", children=[
     html.H1("ebisim dash.", className="mb-3"),
@@ -32,8 +47,7 @@ _FOOTER = html.Section(className="text-justify", children=[
     html.P(_DISCLAIMER, className="text-muted")
 ])
 
-_CONTROLS = html.Div(className="container", children=[
-    html.P("Confirm entries by pressing enter or switching focus."),
+_SIM_CONTROLS = html.Div(className="container", children=[
     html.Div(className="row", children=[
         html.Div(className="col-md mb-3", children=[
             html.Label("Element", htmlFor="ctrl_element"),
@@ -42,7 +56,7 @@ _CONTROLS = html.Div(className="container", children=[
         html.Div(className="col-md mb-3", children=[
             html.Label("Continuous neutral injection", htmlFor="ctrl_cni"),
             dcc.Checklist(id="ctrl_cni", options=[
-                {"label": "Activate CNI", "value": "Active"},
+                {"label": " Activate CNI", "value": "Active"},
             ], values=[], labelStyle={'display': 'inline-block'}, className="form-control")
         ]),
         html.Div(className="col-md mb-3", children=[
@@ -67,40 +81,78 @@ _CONTROLS = html.Div(className="container", children=[
             dcc.Input(id="ctrl_fwhm", value=50, min=0, type="number", className="form-control",
                       debounce=True)
         ])
-    ])
+    ]),
+    html.P("Confirm entries by pressing enter or switching focus."),
 ])
 
 
+#### App Instance and Layout Setting ####
 app = dash.Dash(__name__, external_stylesheets=[_BOOTSTRAP_CDN], url_base_pathname="/ebisim/")
 server = app.server
 
 app.layout = html.Div(className="container-fluid", children=[
     _HEADER,
-    html.Div(className="row", children=[
-        html.Div(className="col", children=[
-            _CONTROLS,
-            dcc.Graph(id='plot_csevo', style={'height': 700},),
+    html.Div(className="col", children=[
+        html.Div(className="row", children=[
+            html.Div(className="col", children=[
+                _SIM_CONTROLS,
+                dcc.Graph(id='plot_csevo', style={"height":700}),
+            ]),
+            html.Div(className="col", children=[
+                html.Label("Distribution plot time (ms)", htmlFor="ctrl_abtime"),
+                dcc.Input(id="ctrl_abtime", value=100, min=0, max=200, type="number",
+                        className="form-control", debounce=True),
+                dcc.Graph(id='plot_distr'),
+                dcc.Graph(id='plot_highest'),
+            ]),
         ]),
-        html.Div(className="col", children=[
-            html.Label("Distribution plot time (ms)", htmlFor="ctrl_abtime"),
-            dcc.Input(id="ctrl_abtime", value=100, min=0, max=200, type="number",
-                      className="form-control"),
-            dcc.Graph(id='plot_distr', style={},),
-            dcc.Graph(id='plot_highest', style={},),
+        html.Div(className="row", children=[
+            html.Div(className="col-md mb-3", children=[
+                dcc.Graph(id="plot_eixs", style={"height":700}),
+            ]),
+            html.Div(className="col-md mb-3", children=[
+                dcc.Graph(id="plot_rrxs", style={"height":700}),
+            ]),
+            html.Div(className="col-md mb-3", children=[
+                dcc.Graph(id="plot_drxs", style={"height":700}),
+            ]),
         ]),
     ]),
     _FOOTER
 ])
 
-@app.callback(Output("ctrl_abtime", "max"), [Input("ctrl_brtime", "value")])
-def limit_slider(tmax):
+
+#### Callback Definition ####
+
+@app.callback(
+    Output("ctrl_abtime", "max"),
+    [Input("ctrl_brtime", "value")]
+)
+def ctrl_abtime_max(tmax):
     return tmax
 
+@app.callback(
+    Output("ctrl_abtime", "value"),
+    [Input("ctrl_brtime", "value")],
+    [State("ctrl_abtime", "value")]
+)
+def ctrl_abtime_clip(tmax, tcur):
+    if tcur < tmax:
+        return tcur
+    else:
+        return tmax
 
-@app.callback(Output("plot_csevo", 'figure'), [
-    Input("ctrl_element", "value"), Input("ctrl_curden", "value"), Input("ctrl_energy", "value"),
-    Input("ctrl_fwhm", "value"), Input("ctrl_brtime", "value"), Input("ctrl_cni", "values")
-])
+@app.callback(
+    Output("plot_csevo", 'figure'),
+    [
+        Input("ctrl_element", "value"),
+        Input("ctrl_curden", "value"),
+        Input("ctrl_energy", "value"),
+        Input("ctrl_fwhm", "value"),
+        Input("ctrl_brtime", "value"),
+        Input("ctrl_cni", "values")
+    ]
+)
 def update_csevo(z, j, e_kin, dr_fwhm, tmax, cni):
     """This function creates the charge state evolution plot"""
     tmax /= 1000
@@ -130,12 +182,10 @@ def update_csevo(z, j, e_kin, dr_fwhm, tmax, cni):
 
     return {"data":data, "layout":layout}
 
-def figure_to_data(figure):
-    return [(np.array(line["x"]), np.array(line["y"])) for line in figure["data"]]
-
-@app.callback(Output("plot_distr", 'figure'), [
-    Input("plot_csevo", 'figure'), Input("ctrl_abtime", "value")
-    ])
+@app.callback(
+    Output("plot_distr", 'figure'),
+    [Input("plot_csevo", 'figure'), Input("ctrl_abtime", "value")]
+)
 def update_distr(csevo, time):
     """This function creates the plot for times with the highest abundance"""
     time /= 1000
@@ -156,7 +206,10 @@ def update_distr(csevo, time):
 
     return {"data":data, "layout":layout}
 
-@app.callback(Output("plot_highest", 'figure'), [Input("plot_csevo", 'figure')])
+@app.callback(
+    Output("plot_highest", 'figure'),
+    [Input("plot_csevo", 'figure')]
+)
 def update_highest(csevo):
     """This function creates the plot for times with the highest abundance"""
     tmax = [t[np.argmax(N)] for (t, N) in figure_to_data(csevo)]
@@ -173,6 +226,85 @@ def update_highest(csevo):
     }
 
     return {"data":data, "layout":layout}
+
+@app.callback(
+    Output("plot_eixs", "figure"),
+    [Input("ctrl_element", "value")]
+)
+def update_eixs(z):
+    try:
+        res = cached_eixs(int(z))
+    except:
+        res = None
+
+    data = []
+    if res:
+        for cs in range(res[1].shape[0]):
+            data.append(
+                {"x": res[0], "y":res[1][cs, :]/1e4, "name":str(cs)+"+", "type":"line"}
+            )
+
+    layout = {
+        "title":'Electron ionisation cross sections',
+        "template":"plotly_dark",
+        "xaxis":{"title":"Electron energy (eV)", "type":"log"},
+        "yaxis":{"title":"Cross section (cm^2)", "type":"log"}
+    }
+
+    return {"data":data, "layout":layout}
+
+@app.callback(
+    Output("plot_rrxs", "figure"),
+    [Input("ctrl_element", "value")]
+)
+def update_rrxs(z):
+    try:
+        res = cached_rrxs(int(z))
+    except:
+        res = None
+
+    data = []
+    if res:
+        for cs in range(res[1].shape[0]):
+            data.append(
+                {"x": res[0], "y":res[1][cs, :]/1e4, "name":str(cs)+"+", "type":"line"}
+            )
+
+    layout = {
+        "title":'Radiative recombination cross sections',
+        "template":"plotly_dark",
+        "xaxis":{"title":"Electron energy (eV)", "type":"log"},
+        "yaxis":{"title":"Cross section (cm^2)", "type":"log"}
+    }
+
+    return {"data":data, "layout":layout}
+
+@app.callback(
+    Output("plot_drxs", "figure"),
+    [Input("ctrl_element", "value"), Input("ctrl_fwhm", "value")]
+)
+def update_drxs(z, fwhm):
+    try:
+        res = cached_drxs(int(z), fwhm)
+    except:
+        res = None
+
+    data = []
+    if res:
+        for cs in range(res[1].shape[0]):
+            data.append(
+                {"x": res[0], "y":res[1][cs, :]/1e4, "name":str(cs)+"+", "type":"line"}
+            )
+
+    layout = {
+        "title":'Dielectronic recombination cross sections',
+        "template":"plotly_dark",
+        "xaxis":{"title":"Electron energy (eV)"},
+        "yaxis":{"title":"Cross section (cm^2)"}
+    }
+
+    return {"data":data, "layout":layout}
+
 
 if __name__ == "__main__":
     app.run_server(debug=True, host="0.0.0.0")
